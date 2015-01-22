ee300af57a72714c6a880aec5151b9255fdf920d
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-01-22 13:42:08 +0200

    Add game data definitions

--- src/Types.hs
+++ src/Types.hs
@@ -1,5 +1,30 @@
 module Types where
 
+import Graphics.Rendering.OpenGL.Raw.Types (GLdouble)
+import Graphics.Rendering.OpenGL.GL.Tensor (Vector3)
 import qualified Graphics.UI.SDL as SDL
 
 type Events = [SDL.Event]
+
+type R = GLdouble
+
+data Point3D = P3D { x :: Integer
+                   , y :: Integer
+                   , z :: Integer
+                   } deriving (Show)
+
+data Level = Level { startingPoint :: Point3D
+                   , endPoint :: Point3D
+                   , obstacles :: [Point3D]
+                   }
+
+data GameState = Game { level :: Level
+                      , rotX :: R
+                      , playerPos :: Vector3 R
+                      }
+
+data ParsedInput = ParsedInput { wCount :: Integer
+                               , aCount :: Integer
+                               , sCount :: Integer
+                               , dCount :: Integer
+                               }
