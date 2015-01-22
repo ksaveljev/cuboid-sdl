27a42457e4e462d37adf95ca77dce5c89aeb38fc
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-01-22 17:14:44 +0200

    Fix `update` stub after parseInput implementation

--- src/Update.hs
+++ src/Update.hs
@@ -9,5 +9,5 @@ import Graphics.Rendering.OpenGL.GL.Tensor (Vector3(..))
 import Types
 
 update :: SF ParsedInput (Bool, GameState)
-update = proc pi@(ParsedInput{ wCount, aCount, sCount, dCount, sdlQuit }) -> do
-  returnA -< (sdlQuit, Game {level = Level (P3D wCount 0 0) (P3D 0 0 0) [], rotX = 0, playerPos = Vector3 0 0 0})
+update = proc input@(ParsedInput{ wCount, aCount, sCount, dCount, sdlQuit }) -> do
+  returnA -< (sdlQuit, Game {level = Level (P3D 0 0 0) (P3D 0 0 0) [], rotX = 0, playerPos = Vector3 0 0 0})
